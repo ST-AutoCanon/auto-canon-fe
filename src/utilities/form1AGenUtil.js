d820c4237f481ef2ac7e3d60d53d6a8210e6a547
sarika-sukalpa <sarikasukalpatechsolutions@gmail.com>
2025-11-12 14:47:10 +0530

    re-pushed frontend updated  code

--- src/utilities/form1AGenUtil.js
+++ src/utilities/form1AGenUtil.js
@@ -90,6 +90,8 @@ if (isTwoWheeler && isThreeWheeler) {
     const dataOfFooterr = footerData.footerData.SealSign.properties;
     let imageUrl;
 
+
+    //updated once
     const fileName = dataOfFooterr.Upload_Seal.file_name;
     imageUrl = `http://bv-reg.com/api/files/downloads/${fileName}`;  // Use the correct backend port
     // console.log("Image loaded successfully:", fileName);
